5f9e6a2b59a024025e9957a8f89454d0357be319
mertins <mertins@gmail.com>
2016-04-01 09:19:39 -0300

    Iniciando

---
+++ BuscaExaustiva/src/main/java/br/com/mm/ufpel/fia/exaustiva/Tabuleiro.java
@@ -0,0 +1,15 @@
+package br.com.mm.ufpel.fia.exaustiva;
+
+/**
+ *
+ * @author mertins
+ */
+public class Tabuleiro {
+
+    private int largura;
+
+    public Tabuleiro(int largura) {
+        this.largura = largura;
+    }
+
+}
