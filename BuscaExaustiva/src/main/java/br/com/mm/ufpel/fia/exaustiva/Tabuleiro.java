15d69ee5e9b3e50a40d6b0da92cb9949593d36cd
mertins <mertins@gmail.com>
2016-04-01 12:20:52 -0300

    Iniciando o desenvolvimento da estrutura

--- BuscaExaustiva/src/main/java/br/com/mm/ufpel/fia/exaustiva/Tabuleiro.java
+++ BuscaExaustiva/src/main/java/br/com/mm/ufpel/fia/exaustiva/Tabuleiro.java
@@ -6,10 +6,38 @@ package br.com.mm.ufpel.fia.exaustiva;
  */
 public class Tabuleiro {
 
-    private int largura;
+    private final int tamanho;
+    private int[][] tabuleiro;
 
-    public Tabuleiro(int largura) {
-        this.largura = largura;
+    public Tabuleiro(int tamanho) {
+        this.tamanho = tamanho;
+        this.build();
+        this.print();
+
     }
 
+    public void build() {
+        tabuleiro = new int[this.tamanho][this.tamanho];
+        int peca = 1;
+        for (int i = 0; i < this.tamanho; i++) {
+            for (int j = 0; j < this.tamanho; j++) {
+                tabuleiro[i][j] = peca++;
+            }
+        }
+        tabuleiro[this.tamanho - 1][this.tamanho - 1] = 0;
+    }
+
+    public void print() {
+        for (int i = 0; i < this.tamanho; i++) {
+            for (int j = 0; j < this.tamanho; j++) {
+                System.out.printf("%d\t", tabuleiro[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Tabuleiro tabuleiro = new Tabuleiro(4);
+
+    }
 }
