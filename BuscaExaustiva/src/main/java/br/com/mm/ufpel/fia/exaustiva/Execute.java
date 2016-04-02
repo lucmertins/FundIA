99b3d2533d5e7800a9e2114f826f04d433c7bbff
mertins <mertins@gmail.com>
2016-04-02 15:33:13 -0300

    Mudando projeto de gradle para maven

--- BuscaExaustiva/src/main/java/br/com/mm/ufpel/fia/exaustiva/Execute.java
+++ BuscaExaustiva/src/main/java/br/com/mm/ufpel/fia/exaustiva/Execute.java
@@ -7,6 +7,18 @@ package br.com.mm.ufpel.fia.exaustiva;
 public class Execute {
 
     public static void main(String[] args) {
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    DepthFirstSearch search = new DepthFirstSearch(3, 20, 100000);
+//                    search.run();
+//                } catch (Exception ex) {
+//                    ex.printStackTrace();
+//                }
+//            }
+//        }).start();
+
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -18,17 +30,5 @@ public class Execute {
                 }
             }
         }).start();
-
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                try {
-//                    DepthFirstSearch search = new DepthFirstSearch(3, 20, 10000);
-//                    search.run();
-//                } catch (Exception ex) {
-//                    ex.printStackTrace();
-//                }
-//            }
-//        }).start();
     }
 }
