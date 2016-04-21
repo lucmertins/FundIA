1d9b39b1166cf4cab7449284087cc1ec73d0ec29
mertins <mertins@gmail.com>
2016-04-21 11:20:18 -0300

    Trabalhando minimax

--- BuscaCompetitiva/src/main/java/br/com/mertins/ufpel/competitiva/util/BoardTicTacToe.java
+++ BuscaCompetitiva/src/main/java/br/com/mertins/ufpel/competitiva/util/BoardTicTacToe.java
@@ -32,7 +32,22 @@ public class BoardTicTacToe {
     }
 
     public boolean terminal(Node node) {
-        return false;
+        byte[][] situacao = this.buildBoard(node);
+        boolean espacoLivre=false;
+        for (int y = 0; y < tabuleiro.length; y++) {
+            for (int x = 0; x < tabuleiro.length; x++) {
+                if ((tabuleiro[y][0].getMarker() == tabuleiro[y][1].getMarker() && tabuleiro[y][1].getMarker() == tabuleiro[y][2].getMarker())
+                        || (tabuleiro[0][x].getMarker() == tabuleiro[1][x].getMarker() && tabuleiro[1][x].getMarker() == tabuleiro[2][x].getMarker())
+                        || (tabuleiro[0][0].getMarker() == tabuleiro[1][1].getMarker() && tabuleiro[1][1].getMarker() == tabuleiro[2][2].getMarker())
+                        || (tabuleiro[2][0].getMarker() == tabuleiro[1][1].getMarker() && tabuleiro[1][1].getMarker() == tabuleiro[0][2].getMarker())) {
+                    return true;
+                }
+                if (tabuleiro[y][x].getMarker() == Node.Marker.B) {
+                    espacoLivre=true;
+                }
+            }
+        }
+        return !espacoLivre;
     }
 
     public Node heuristic(Node node) {
@@ -40,7 +55,27 @@ public class BoardTicTacToe {
     }
 
     public boolean gameOver(Node node) {
-        return false; //avaliar se impate, ganhou player, ganho adversario
+        boolean espacoLivre=false;
+        for (int y = 0; y < tabuleiro.length; y++) {
+            for (int x = 0; x < tabuleiro.length; x++) {
+                if ((tabuleiro[y][0].getMarker() == tabuleiro[y][1].getMarker() && tabuleiro[y][1].getMarker() == tabuleiro[y][2].getMarker() && tabuleiro[y][2].getMarker()==Node.Marker.X)
+                        || (tabuleiro[0][x].getMarker() == tabuleiro[1][x].getMarker() && tabuleiro[1][x].getMarker() == tabuleiro[2][x].getMarker()&&tabuleiro[2][x].getMarker()==Node.Marker.X)
+                        || (tabuleiro[0][0].getMarker() == tabuleiro[1][1].getMarker() && tabuleiro[1][1].getMarker() == tabuleiro[2][2].getMarker()&&tabuleiro[2][2].getMarker()==Node.Marker.X)
+                        || (tabuleiro[2][0].getMarker() == tabuleiro[1][1].getMarker() && tabuleiro[1][1].getMarker() == tabuleiro[0][2].getMarker()&&tabuleiro[0][2].getMarker()==Node.Marker.X)) {
+                    return true;  //ganhou 
+                }
+                if ((tabuleiro[y][0].getMarker() == tabuleiro[y][1].getMarker() && tabuleiro[y][1].getMarker() == tabuleiro[y][2].getMarker() && tabuleiro[y][2].getMarker()==Node.Marker.O)
+                        || (tabuleiro[0][x].getMarker() == tabuleiro[1][x].getMarker() && tabuleiro[1][x].getMarker() == tabuleiro[2][x].getMarker()&&tabuleiro[2][x].getMarker()==Node.Marker.O)
+                        || (tabuleiro[0][0].getMarker() == tabuleiro[1][1].getMarker() && tabuleiro[1][1].getMarker() == tabuleiro[2][2].getMarker()&&tabuleiro[2][2].getMarker()==Node.Marker.O)
+                        || (tabuleiro[2][0].getMarker() == tabuleiro[1][1].getMarker() && tabuleiro[1][1].getMarker() == tabuleiro[0][2].getMarker()&&tabuleiro[0][2].getMarker()==Node.Marker.O)) {
+                    return true;  //perdeu
+                }
+                if (tabuleiro[y][x].getMarker() == Node.Marker.B) {
+                    return espacoLivre=true;
+                }
+            }
+        }
+        return !espacoLivre;   //se não tem espaco livre e empate 
     }
 
     private void clear() {
@@ -49,5 +84,18 @@ public class BoardTicTacToe {
                 tabuleiro1[x] = new Node(Node.Infinite.POSITIVE, Node.Marker.B);
             }
         }
+    }
+
+    private byte[][] buildBoard(Node node) {
+        byte[][] board = new byte[3][3];
+        if (node.getMarker() != Node.Marker.B) {
+            board[node.getPosY()][node.getPosX()] = (byte) node.getMarker().toByte();
+        }
+        Node temp = node.getParent();
+        while (temp != null) {
+            board[temp.getPosY()][temp.getPosX()] = (byte) temp.getMarker().toByte();
+            temp = node.getParent();
+        }
+        return board;
     }
 }
--- BuscaCompetitiva/src/main/java/br/com/mertins/ufpel/competitiva/util/Node.java
+++ BuscaCompetitiva/src/main/java/br/com/mertins/ufpel/competitiva/util/Node.java
@@ -26,11 +26,35 @@ public class Node {
                     return "H";
             }
         }
+
+        public char toChar() {
+            switch (this) {
+                case X:
+                    return 'X';
+                case O:
+                    return '0';
+                default:
+                    return 'H';
+            }
+        }
+        public char toByte() {
+            switch (this) {
+                case X:
+                    return 1;
+                case O:
+                    return 2;
+                default:
+                    return 0;
+            }
+        }
     }
     private final Long value;
     private final Marker marker;
     private final Infinite infinite;
     private final List<Node> children = new ArrayList<>();
+    private Node parent = null;
+    private byte posX;
+    private byte posY;
 
     public Node(Long value, Marker marker) {
         this.value = value;
@@ -60,6 +84,22 @@ public class Node {
         return children;
     }
 
+    public byte getPosX() {
+        return posX;
+    }
+
+    public void setPosX(byte posX) {
+        this.posX = posX;
+    }
+
+    public byte getPosY() {
+        return posY;
+    }
+
+    public void setPosY(byte posY) {
+        this.posY = posY;
+    }
+
     public Node max(Node other) {
         return this.value >= other.value ? this : other;
     }
@@ -69,6 +109,12 @@ public class Node {
     }
 
     public void addChild(Node node) {
+        node.parent = this;
         children.add(node);
     }
+
+    public Node getParent() {
+        return parent;
+    }
+    
 }
